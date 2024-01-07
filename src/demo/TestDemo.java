649f906f1655db08e71d21b11d6b18475aec81ef
mustafaalani <mustafaalani@192.168.0.233>
2024-01-06 18:40:58 -0700

    Initial Commit

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -10,6 +10,11 @@ public class TestDemo {
 		System.out.println("QA");
 		
 		System.out.println("Day3");
+		
+		 System.out.println("Practice...");
+		 System.out.println("Today is not Tuesday");
+		 
+		 System.out.println("Learn Github");
 	}
 	//This is coming from Github
 	//Today is friday
